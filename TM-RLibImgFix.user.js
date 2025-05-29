77b3caeba6ed7201dd6d732f80c2c4bc5c884a22
TranslatorGen13 <164593901+TranslatorGen13@users.noreply.github.com>
2025-05-29 21:05:14 +0300

    Update TM-RLibImgFix.user.js
    Исправление ошибок в коде, а также добавление новой функции — исправление отображения панели переключения глав поверх верхнего выпадающего меню и контекстного меню

--- TM-RLibImgFix.user.js
+++ TM-RLibImgFix.user.js
@@ -2,8 +2,8 @@
 // @name         TM-RLibImgFix
 // @name:ru      Починка иллюстраций на ренобелибе
 // @namespace    http://tampermonkey.net/
-// @version      0.0.2
-// @description:ru  Чинит отображение иллюстраций на ренобелибе и ссылки из закреплённых коментариев с помощью замены абсолютных ссылок на относительные
+// @version      0.0.3
+// @description:ru  Чинит отображение иллюстраций на ренобелибе и ссылки из закреплённых коментариев с помощью замены абсолютных ссылок на относительные, а также исправляет отображение панели переключения глав
 // @author       TranslatorGen13
 // @match        https://*.novelslib.me/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ranobelib.me
@@ -15,6 +15,13 @@
 (function() {
 
     const oldUrl = 'https://ranobelib.me';
+    function FixLayer() {
+        let elem = document.querySelector('#app').children[1].childNodes[7];
+        if (elem!=null) {
+            elem.insertAdjacentHTML('beforeBegin','<div id="BtnLayerFix"></div>');
+            document.querySelector('#BtnLayerFix').appendChild(elem);
+        };
+    };
     function ReplaceUrl() {
         document.querySelectorAll('img[src^="' + oldUrl + '"]').forEach(img => {
             img.src = img.src.replace(oldUrl, '');
@@ -22,9 +29,10 @@
         document.querySelectorAll('a[href^="' + oldUrl + '"]').forEach(a => {
             a.href = a.href.replace(oldUrl, '');
         });
+        FixLayer();
     }
     setInterval(function() {
-        if (document.querySelector('img[src^="' + oldUrl + '"]')!=null || document.querySelectorAll('a[href^="' + oldUrl + '"]')!=null){
+        if (document.querySelector('img[src^="' + oldUrl + '"]')!=null || document.querySelectorAll('a[href^="' + oldUrl + '"]').length>0 || document.querySelector('#BtnLayerFix')==null){
             ReplaceUrl();
         }
     }, 1000)
